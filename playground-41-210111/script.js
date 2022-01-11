9cefa7a4e3098f36532d66b70e8f50b9579e56ee
matthiasmeissen <meissenmatthias@gmail.com>
2022-01-11 22:56:24 +0100

    Completed Playground 41

---
+++ playground-41-210111/script.js
@@ -0,0 +1,38 @@
+const contentTag = document.querySelector('div.content')
+const editTag = document.querySelector('div.top a')
+
+editTag.addEventListener('click', function () {
+    removeDividers()
+})
+
+const createItem = function (location, text) {
+    var item = document.createElement('div')
+    item.classList.add('item')
+    item.innerHTML = `<p>${text}</p>`
+
+    contentTag.insertBefore(createDivider(), location)
+    contentTag.insertBefore(item, location)
+}
+
+const createDivider = function () {
+    var item = document.createElement('div')
+    item.classList.add('insert')
+    item.innerHTML = '<div><span class="tooltip"><input></input></span></div>'
+
+    item.querySelector('input').addEventListener('change', function (event) {
+        createItem(this.parentElement.parentElement.parentElement, event.target.value)
+        event.target.value = ''
+    })
+
+    return item
+}
+
+const removeDividers = function () {
+    const dividers = document.querySelectorAll('div.insert')
+
+    dividers.forEach(divider => {
+        divider.remove()
+    });
+}
+
+contentTag.appendChild(createDivider())
