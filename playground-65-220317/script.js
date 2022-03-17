4ffdfacd6c244e41823e05a4456d2149588414b4
matthiasmeissen <meissenmatthias@gmail.com>
2022-03-18 00:08:41 +0100

    Completed Playground 65

---
+++ playground-65-220317/script.js
@@ -0,0 +1,17 @@
+const headerTag = document.querySelector('div.header')
+const cursorTag = document.querySelector('div.cursor')
+const tags = document.querySelectorAll('div.pointer')
+
+window.addEventListener('mousemove', function (event) {
+    cursorTag.style.transform = `translate(${event.pageX - cursorTag.clientWidth / 2}px, ${event.pageY - cursorTag.clientHeight / 2}px)`
+})
+
+tags.forEach(item => {
+    item.addEventListener('mouseenter', function () {
+        cursorTag.children[0].style.transform = 'scale(0)'
+    })
+
+    item.addEventListener('mouseleave', function () {
+        cursorTag.children[0].style.transform = 'scale(1)'
+    })
+});
